68a403e04341a306b8aa2bd25599108f3b22d196
Meriç Mert Bulca <meric.bulca@obss.tech>
2025-04-27 06:15:58 +0300

    chore(husky): added  husky pre-commit hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 
 export default function Home() {
+  const some = 3
+  console.log(some);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -17,6 +19,7 @@ export default function Home() {
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
               src/app/page.tsx
+              {some}
             </code>
             .
           </li>
